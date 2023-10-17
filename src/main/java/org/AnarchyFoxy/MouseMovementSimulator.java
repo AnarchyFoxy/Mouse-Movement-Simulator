ac93df06902724b1e45c7cc9281a46e89e17483b
Astryda Malinowska <astamalinowska@outlook.com>
2023-10-17 15:09:24 +0200

    Added repetition mouse movement for one hour

--- src/main/java/org/AnarchyFoxy/MouseMovementSimulator.java
+++ src/main/java/org/AnarchyFoxy/MouseMovementSimulator.java
@@ -17,23 +17,29 @@ public class MouseMovementSimulator {
             int startX = (int) java.awt.MouseInfo.getPointerInfo().getLocation().getX();
             int startY = (int) java.awt.MouseInfo.getPointerInfo().getLocation().getY();
 
-            // Symuluj ruch myszką w górę
-            for (int i = 0; i < distance; i++) {
-                robot.mouseMove(startX, startY - i);
-                try {
-                    Thread.sleep(duration / distance);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
+            // Czas trwania symulacji w milisekundach (np. 1 godzina)
+            int simulationDuration = 3600000;
 
-            // Symuluj ruch myszką w dół
-            for (int i = 0; i < distance; i++) {
-                robot.mouseMove(startX, startY - distance + i);
-                try {
-                    Thread.sleep(duration / distance);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+            long startTime = System.currentTimeMillis();
+            while (System.currentTimeMillis() - startTime < simulationDuration) {
+                // Symuluj ruch myszką w górę
+                for (int i = 0; i < distance; i++) {
+                    robot.mouseMove(startX, startY - i);
+                    try {
+                        Thread.sleep(duration / distance);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                // Symuluj ruch myszką w dół
+                for (int i = 0; i < distance; i++) {
+                    robot.mouseMove(startX, startY - distance + i);
+                    try {
+                        Thread.sleep(duration / distance);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         } catch (AWTException e) {
